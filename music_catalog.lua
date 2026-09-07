749c93da8de55cf14a4629049e536dbc0a04df94
MrRos3 <114161345+MrRos3@users.noreply.github.com>
2026-09-07 16:05:36 +0400

    Add bundled music catalog

---
+++ music_catalog.lua
@@ -0,0 +1,22 @@
+-- VantaTest bundled music catalog.
+-- Remote URLs are filled after the user-connected file host returns stable links.
+return {
+    {
+        Key = "azeri-kavkaz",
+        Title = "Azeri Kavkaz",
+        Artist = "Caucasus Dance",
+        AudioFile = "Azeri Kavkaz.mp3",
+        CoverFile = "Azeri Kavkaz.jpg",
+        AudioURL = "",
+        CoverURL = "",
+    },
+    {
+        Key = "i-love-you-so-arabic-slowed",
+        Title = "I Love You So (Arabic Version - Slowed)",
+        Artist = "aessy • Tom Vaulbert",
+        AudioFile = "I Love You So (Arabic Version - Slowed).mp3",
+        CoverFile = "I Love You So (Arabic Version - Slowed).jpg",
+        AudioURL = "",
+        CoverURL = "",
+    },
+}
