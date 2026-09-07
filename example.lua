27ceda4754003a5d04988edc43d870447e22cab7
MrRos3 <114161345+MrRos3@users.noreply.github.com>
2026-09-07 14:35:39 +0400

    Use premium music player showcase

--- example.lua
+++ example.lua
@@ -4,7 +4,7 @@ local VantaUI = loadstring(game:HttpGet(
 ))()
 
 local CompactMusic = loadstring(game:HttpGet(
-    "https://raw.githubusercontent.com/MrRos3/VantaTest/main/musicplayer_polished.lua?v=" .. cacheBuster
+    "https://raw.githubusercontent.com/MrRos3/VantaTest/main/musicplayer_premium.lua?v=" .. cacheBuster
 ))()
 CompactMusic:Init(VantaUI, { Folder = "VantaTest/Music" })
 
@@ -163,7 +163,7 @@ Home:Button({
     Icon = "sparkles",
     Callback = function()
         VantaUI:Notify({
-            Content = "VantaTest polished glass music-player build is running 🎵",
+            Content = "VantaTest premium music-player build is running 🎵",
             Icon = "music",
         })
     end,
