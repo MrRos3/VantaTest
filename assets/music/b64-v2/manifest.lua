902976825be7909512b3fe4213cdbfebbd321f5a
MrRos3 <114161345+MrRos3@users.noreply.github.com>
2026-09-09 01:16:56 +0400

    Add direct Vanta music bundle manifest

---
+++ assets/music/b64-v2/manifest.lua
@@ -0,0 +1,43 @@
+return {
+    version = "2026-09-09-direct-mp3-v1",
+    tracks = {
+        {
+            key = "azeri-kavkaz",
+            title = "Azeri Kavkaz",
+            artist = "Caucasus Dance",
+            local_stem = "Vanta Builtin - Azeri Kavkaz",
+            audio = {
+                ext = "mp3",
+                path = "azeri-kavkaz/audio",
+                chunks = 25,
+                bytes = 598268,
+                sha256 = "00c85f1415e477a48996ddb841bd56fbbc8865bfc60ed57ef8cad04d60705d9f",
+            },
+            cover = {
+                ext = "jpg",
+                path = "azeri-kavkaz/cover",
+                chunks = 1,
+                bytes = 0,
+            },
+        },
+        {
+            key = "i-love-you-so-arabic-slowed",
+            title = "I Love You So (Arabic Version - Slowed)",
+            artist = "aessy • Tom Vaulbert",
+            local_stem = "Vanta Builtin - I Love You So (Arabic Version - Slowed)",
+            audio = {
+                ext = "mp3",
+                path = "i-love-you-so-arabic-slowed/audio",
+                chunks = 21,
+                bytes = 481707,
+                sha256 = "d0e2b86ad07968e15bea8908e1572640382ebf9793ad3a436b78b32fc970b461",
+            },
+            cover = {
+                ext = "jpg",
+                path = "i-love-you-so-arabic-slowed/cover",
+                chunks = 1,
+                bytes = 0,
+            },
+        },
+    },
+}
